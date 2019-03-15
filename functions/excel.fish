7b5391da3d17553763fc4a071055bcc87af860d7
yamatakau08 <yamatakau08@gmail.com>
2019-03-15 13:49:30 +0900

    add case 'MINGW64_NT-*'

--- functions/excel.fish
+++ functions/excel.fish
@@ -1,5 +1,7 @@
 function excel
     switch (uname)
+    case 'MINGW64_NT-*'
+         /c/Program\ Files\ \(x86\)/Microsoft\ Office/Office14/EXCEL.EXE &
     case 'CYGWIN_NT-*' # 6.1,6.3
     	 set file (cygpath -w $argv)
 	 command /cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ Office/Office14/EXCEL.EXE $file &
