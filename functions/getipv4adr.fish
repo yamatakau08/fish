6a77c5b9cb9b7721e3e4f60ba397462bc12a132b
yamatakau08 <yamatakau08@gmail.com>
2020-01-08 11:42:44 +0900

    support setting for proxy environment variable on private/company network.

--- functions/getipv4adr.fish
+++ functions/getipv4adr.fish
@@ -1,9 +1,9 @@
 function getipv4adr
-    switch (uname -s)
+    switch (uname)
     case Darwin
-        set ipv4info (ifconfig | grep -e 'inet ' | grep -v '127.0.0.1' | string split ' ')
-	set ipv4adr  $ipv4info[2]
     case Linux
+        set ipv4info (ifconfig | grep -e 'inet ' | grep -v '127.0.0.1' | string trim -l | string split ' ')
+	set ipv4adr  $ipv4info[2]
     case 'MINGW64_NT-*'
     case '*'
         echo "add 'uname' label in ~/.config/fish/functions/networkchk.fish"
