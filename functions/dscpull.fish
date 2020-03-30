86312c0d396348520fa3b61ad04180fc0a9f4d94
yamatakau08 <yamatakau08@gmail.com>
2020-03-30 14:39:15 +0900

    .

---
+++ functions/dscpull.fish
@@ -0,0 +1,16 @@
+# https://qiita.com/TNaruto/items/b2407f5668e15e42bedd
+function dscpull
+  switch (uname -s)
+    case 'MINGW*'
+     set dscmovf //storage/0123-4567/DCIM/100ANDRO/$argv[1]
+    case '*'
+     set dscmovf /storage/0123-4567/DCIM/100ANDRO/$argv[1]
+    end
+
+  # list android devices to use
+  adb devices -l
+
+  read -P "select transport_id: " tid
+
+  adb -t $tid pull $dscmovf $argv[2]
+end
