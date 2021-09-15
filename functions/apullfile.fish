dfa6fdf1f53530b939410ab87e3ce0d9737aa9cd
yamatakau08 <yamatakau08@gmail.com>
2021-09-15 15:25:01 +0900

    fixed to pull file specified absolute path

--- functions/apullfile.fish
+++ functions/apullfile.fish
@@ -1,16 +1,17 @@
-# https://qiita.com/TNaruto/items/b2407f5668e15e42bedd
 function apullfile
   switch (uname -s)
     case 'MINGW*'
-     set dscmovf //storage/0123-4567/DCIM/100ANDRO/$argv[1]
+     set pullfile /$argv[1]
     case '*'
-     set dscmovf /storage/0123-4567/DCIM/100ANDRO/$argv[1]
+     set pullfile $argv[1]
   end
+
+  echo $pullfile
 
   # list android devices to use
   adb devices -l
 
   read -P "select transport_id: " tid
 
-  adb -t $tid pull $dscmovf
+  adb -t $tid pull $pullfile
 end
