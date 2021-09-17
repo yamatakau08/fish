2d4def0515309febe7534678f9e8cf75e2172ad9
yamatakau08 <yamatakau08@gmail.com>
2021-09-17 21:56:14 +0900

    .

--- functions/ascreenshot.fish
+++ functions/ascreenshot.fish
@@ -3,43 +3,42 @@
 # dpngf : android device side png file
 
 function ascreenshot
-  # list android devices
-  adb devices -l
+    # list android devices
+    adb devices -l
 
-  read -P "select transport_id: " tid
+    read -P "select transport_id: " tid
 
-  # get ro.product.model for specifying DSC path
-  set tmodel (adb -t $tid shell "getprop ro.product.model" 2> /dev/null)
-  set model  (string trim -c '' $tmodel) # important to strip ^M
+    # get ro.product.model for specifying DSC path
+    set tmodel (adb -t $tid shell "getprop ro.product.model" 2> /dev/null)
+    set model  (string trim -c '' $tmodel) # important to strip ^M
 
-  # check if model is supported
-  set pngf screenshot.png
+    # check if model is supported
+    set pngf screenshot.png
 
-  switch $model
-    case 'SOV35' 'moto g(30)'
-      set dpngf //sdcard/Download/$pngf # // on MSYS2 environment, but available on Darwin...
-    case '*'
-      echo $model is not supported
-      return
-  end
+    switch $model
+	case 'SOV35' 'moto g(30)'
+	    set dpngf //sdcard/Download/$pngf # // on MSYS2 environment, but available on Darwin...
+	case '*'
+	    echo $model is not supported
+	    return
+    end
 
-  echo "screenshot $pngf ..."
-  adb -t $tid shell screencap -p $dpngf
-  adb -t $tid pull  $dpngf $argv[-1] # if the local file name is not specified, local file name is $pngf
-  adb -t $tid shell rm $dpngf # remove screenshot file in device
+    if test -z $argv[-1]
+	set lpngf (basename $pngf .png)_(date +%y%m%d_%H%M%S).png
+    else
+	set lpngf $argv[-1]
+    end
 
-  # view the file pulled
-  if test -z $argv[-1]
-    set sfile $pngf
-  else
-    set sfile $argv[-1]
-  end
+    echo "screenshot to $lpngf ..."
+    adb -t $tid shell screencap -p $dpngf
+    adb -t $tid pull  $dpngf $lpngf
+    adb -t $tid shell rm $dpngf # remove screenshot file in device
 
-  # open the file
-  switch (uname)
-    case 'Darwin'
-      open  $sfile
-    case '*'
-      start $sfile
-  end
+    # open the file
+    switch (uname)
+	case 'Darwin'
+	    open  $lpngf
+	case '*'
+	    start $lpngf
+    end
 end
