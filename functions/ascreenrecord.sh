bc562a63cde383727f148d7da6eec869d59f3d25
yamatakau08 <yamatakau08@gmail.com>
2021-10-19 17:05:29 +0900

    enable to open the MOV,mp4 file which has space in file name

--- functions/ascreenrecord.sh
+++ functions/ascreenrecord.sh
@@ -6,16 +6,19 @@ adb devices -l
 echo -n "select transport_id: "
 read tid
 
-mp4f="//sdcard/Download/screenrecord.mp4"
+dmp4f="//sdcard/Download/screenrecord.mp4"
+## ${mp4f##*.}
+## https://stackoverflow.com/a/3298229
+lmpf4="$(basename $dmp4f .mp4)_$(date +%y%m%d_%H%M%S).${dmp4f##*.}"
 
 trap 'func_trap ; exit' INT
 
 function func_trap
 {
     sleep 1
-    adb -t $tid pull $mp4f
+    adb -t $tid pull $dmp4f $lmpf4
 }
 
 ##
-echo "start screenrecord ..."
-adb -t $tid shell screenrecord $mp4f
+echo "start screenrecord to $lmpf4"
+adb -t $tid shell screenrecord $dmp4f
