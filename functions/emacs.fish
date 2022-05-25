6a75fb9ae75d4fded59987068073f31f5504b4b3
yamatakau08 <yamatakau08@gmail.com>
2022-05-26 08:58:30 +0900

    fix emacs launch command on MSYS evironment

--- functions/emacs.fish
+++ functions/emacs.fish
@@ -9,15 +9,15 @@ function emacs
 	    /usr/local/bin/emacs $argv
 	case 'MINGW*' 'MSYS*'
 	    # https://ayatakesi.github.io/emacs/temp/html/Windows-Startup.html
-	    # runemacs.exe CAN     execute other commands on mintty
-	    # emacs.exe    CAN NOT execute other commands on mintty
+	    # After launch runemacs.exe on mintty, CAN     execute other commands on mintty
+	    # After launch emacs.exe    on mintty, CAN NOT execute other commands on mintty
 	    #	/c/msys64/mingw64/bin/runemacs           $argv # emacs self compiled somehow google-translate doesn't work
 	    #	/c/winbin/emacs-26.2-x86_64/bin/runemacs $argv # use gnu ftp binary
 	    #	/c/winbin/emacs-26.3-x86_64/bin/runemacs $argv # use gnu ftp binary
 	    #	runemacs $argv # run each GUI emacs on mingw32/64, my a part of helm-anki-browse doesn't work on mingw32
 	    #	/c/winbin/emacs-28.0.50-snapshot-2020-07-05-x86_64/bin/runemacs.exe $argv
 	    #   /c/winbin/emacs-28.0.50/x86_64/bin/runemacs.exe $argv
-	    /c/winbin/emacs-28.0.50-snapshot/bin/runemacs.exe $argv
+	    /c/winbin/emacs-29.0.50-snapshot/bin/runemacs.exe $argv
 	case '*'
 	    echo "add 'uname' label in ~/.config/fish/functions/emacs.fish"
     end
