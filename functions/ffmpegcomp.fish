8c033f7442ee0c6ceb8c50f57c91ab5e071ca4f7
yamatakau08 <yamatakau08@gmail.com>
2022-06-14 13:12:23 +0900

    support cygwin environment and refactor.

--- functions/ffmpegcomp.fish
+++ functions/ffmpegcomp.fish
@@ -1,10 +1,14 @@
+# Since cygwin doesn't have ffmpeg package, use windows native ffmpeg.exe
 function ffmpegcomp
   switch (uname -s)
     case 'MINGW*'
-     # set ffmpeg /c/winbin/ffmpeg-20200816-5df9724-win64-static/bin/ffmpeg.exe
-     #$ffmpeg -i $argv[1] -crf 30 (basename $argv[1] .mp4)_comp.mp4
-     ffmpeg -i $argv[1] -crf 30 (basename $argv[1] .mp4)_comp.mp4
-    case *
-     echo "not supported!"
+      set ffmpeg "//c/winbin/ffmpeg-master-latest-win64-gpl-shared/bin/ffmpeg.exe"
+    case 'CYGWIN_NT*'
+      set ffmpeg "/cygdrive/c/winbin/ffmpeg-master-latest-win64-gpl-shared/bin/ffmpeg.exe"
+    case '*'
+      echo "not supported!"
+      exit
     end
+
+    $ffmpeg -i $argv[1] -crf 30 (basename $argv[1] .mp4)_comp.mp4
 end
