c0815946ece4e96e456aab1196d75a00795dc1a3
yamatakau08 <yamatakau08@gmail.com>
2023-03-28 17:54:10 +0900

    support "-nw" option to pass emacs without background on terminal mode

--- functions/emacs.fish
+++ functions/emacs.fish
@@ -1,27 +1,48 @@
 function emacs
+    set nw false
+    ## check if "-nw" option is specified
+    for i in (seq (count $argv))
+	#echo -n $argv[$i]
+	if test (string match -- "-nw" $argv[$i])
+	    set nw true
+	    break
+	end
+    end
+
     switch (uname -s)
+	## Note on Windows Environment
+	# https://ayatakesi.github.io/emacs/temp/html/Windows-Startup.html
+	# After launch runemacs.exe on mintty, CAN     execute other commands on mintty
+	# After launch emacs.exe    on mintty, CAN NOT execute other commands on mintty
+
 	case 'CYGWIN*'
-	    #/cygdrive/c/winbin/emacs-29.0.50-snapshot/bin/runemacs.exe $argv &
-	    #/cygdrive/c/winbin/emacs-28.0.91/bin/runemacs.exe $argv &
-	    #/cygdrive/c/winbin/emacs-28.1.90/bin/runemacs.exe $argv &
-	    /cygdrive/c/winbin/emacs-29.0.60/bin/runemacs.exe $argv &
+	    if test $nw
+		/cygdrive/c/winbin/emacs-29.0.60/bin/emacs.exe $argv
+	    else
+		/cygdrive/c/winbin/emacs-29.0.60/bin/runemacs.exe $argv &
+	    end
 	case Darwin
-	    /Applications/Emacs.app/Contents/MacOS/Emacs $argv &
+	    if test $nw
+		/Applications/Emacs.app/Contents/MacOS/Emacs $argv
+	    else
+		/Applications/Emacs.app/Contents/MacOS/Emacs $argv &
+	    end
 	case Linux
-	    #       /usr/local/bin/emacs $argv & # if '&' exists, "emacs -nw" launch as backgroud, it's not good
-	    /usr/bin/emacs $argv &
+	    if test $nw
+		/usr/bin/emacs $argv &
+	    else
+		/usr/bin/emacs $argv &
+	    end
 	case 'MINGW*' 'MSYS*'
-	    # https://ayatakesi.github.io/emacs/temp/html/Windows-Startup.html
-	    # After launch runemacs.exe on mintty, CAN     execute other commands on mintty
-	    # After launch emacs.exe    on mintty, CAN NOT execute other commands on mintty
-	    #	/c/msys64/mingw64/bin/runemacs           $argv # emacs self compiled somehow google-translate doesn't work
-	    #	/c/winbin/emacs-26.2-x86_64/bin/runemacs $argv # use gnu ftp binary
-	    #	/c/winbin/emacs-26.3-x86_64/bin/runemacs $argv # use gnu ftp binary
-	    #	runemacs $argv # run each GUI emacs on mingw32/64, my a part of helm-anki-browse doesn't work on mingw32
-	    #	/c/winbin/emacs-28.0.50-snapshot-2020-07-05-x86_64/bin/runemacs.exe $argv
-	    #   /c/winbin/emacs-28.0.50/x86_64/bin/runemacs.exe $argv
-	    /c/winbin/emacs-29.0.50-snapshot/bin/runemacs.exe $argv
+	    #	/c/msys64/mingw64/bin/runemacs $argv # emacs self compiled somehow google-translate doesn't work
+	    #	runemacs $argv                       # run each GUI emacs on mingw32/64, my a part of helm-anki-browse doesn't work on mingw32
+	    if test $nw
+		/c/winbin/emacs-29.0.50-snapshot/bin/runemacs.exe $argv
+	    else
+		/c/winbin/emacs-29.0.50-snapshot/bin/runemacs.exe $argv &
+	    end
 	case '*'
 	    echo "add 'uname' label in ~/.config/fish/functions/emacs.fish"
     end
+
 end
