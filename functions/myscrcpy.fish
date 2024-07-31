c8178618e0d6c8d9adf2f8337738b4058fd8e990
yamatakau08 <yamatakau08@gmail.com>
2024-08-01 00:42:02 +0900

    fix the scrcpy launch error in case of adb Wi-Fi connect.

--- functions/myscrcpy.fish
+++ functions/myscrcpy.fish
@@ -12,21 +12,16 @@ function myscrcpy
     read -P "select transport_id: " tid
 
     ## get ro.serialno
-    set serialno (adb -t $tid shell "getprop ro.serialno" 2> /dev/null)
-    set serialno (string trim -c '' $serialno) # important to strip ^M
+    set serial_no (adb devices -l | grep -i "transport_id:$tid")
+    set serial_no (string split ' ' $serial_no)
+    set serial_no $serial_no[1]
 
     ## get ro.product.model
-    set model (adb -t $tid shell "getprop ro.product.model" 2> /dev/null)
+    set model (adb -t $tid shell "getprop ro.product.model")
     set model (string trim -c '' $model) # important to strip ^M
 
-    # https://unix.stackexchange.com/questions/317330/check-a-range-with-switch-case-in-fish-shell
-    if string match --ignore-case --regex 'command not found*' $serialno > /dev/null
-	echo "scrcpy: tid $tid is not supported!"
-	return
-    end
-
     ## check os type
-    set xuname (uname) > /dev/null
+    set xuname (uname)
 
     if string match --ignore-case --regex 'CYGWIN' $xuname
 	set cmdscrcpy ~/OneDrive/archive/scrcpy-win64-v2.4/scrcpy-win64-v2.4/scrcpy.exe
@@ -37,15 +32,17 @@ function myscrcpy
 	set screen_record_dir ~/tmp/scrcpy_record
 	set screen_record_file $screen_record_dir/$model-(date +'%Y%m%d-%H%M%S').mp4
     else
+	echo $xuname is not supported > /dev/stderr
 	return
     end
 
-    # need make directory to store
+    # need to make directory to store the screen copy file
     if not test -d $screen_record_dir
 	mkdir $screen_record_dir
     end
 
-    $cmdscrcpy --serial $serialno --video-codec=h264 --max-size=1920 --max-fps=60 --keyboard=uhid --show-touches --record=$screen_record_file --verbosity=verbose
+    ## scrcpy execute
+    $cmdscrcpy --serial $serial_no --video-codec=h264 --max-size=1920 --max-fps=60 --keyboard=uhid --show-touches --record=$screen_record_file --verbosity=verbose
     # --no-audio
     # --window-width 1000 --window-height 2500
 
