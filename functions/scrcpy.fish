3900de9308b5783c9453aa2f9852517e11846682
yamatakau08 <yamatakau08@gmail.com>
2024-07-31 15:27:44 +0900

    add judging os and refactor.

--- functions/scrcpy.fish
+++ functions/scrcpy.fish
@@ -5,27 +5,40 @@
 # Moto g53J INFO: Texture: 720x1600
 
 function scrcpy
-    # list android devices
+
+    ## check os type
+    set xuname (uname) > /dev/null
+
+    if string match --ignore-case --regex 'CYGWIN' $xuname
+	set cmdscrcpy ~/OneDrive/archive/scrcpy-win64-v2.4/scrcpy-win64-v2.4/scrcpy.exe
+	set screen_record_dir c:\\yama\\OneDrive\\tmp\\scrcpy_record
+	set screen_record_file $screen_record_dir/$model-(date +'%Y%m%d-%H%M%S').mp4
+    else if string match --ignore-case --regex 'Darwin' $xuname
+	set cmdscrcpy scrcpy
+	set screen_record_dir ~/tmp/scrcpy_record
+	set screen_record_file $screen_record_dir/$model-(date +'%Y%m%d-%H%M%S').mp4
+    else
+	return
+    end
+
+    ## list android devices
     adb devices -l
 
     read -P "select transport_id: " tid
 
-    # get ro.serialno
-    set tserialno (adb -t $tid shell "getprop ro.serialno" 2> /dev/null)
-    set serialno  (string trim -c '' $tserialno) # important to strip ^M
+    ## get ro.serialno
+    set serialno (adb -t $tid shell "getprop ro.serialno" 2> /dev/null)
+    set serialno (string trim -c '' $serialno) # important to strip ^M
 
-    set tmodel (adb -t $tid shell "getprop ro.product.model" 2> /dev/null)
-    set model  (string trim -c '' $tmodel) # important to strip ^M
+    ## get ro.product.model
+    set model (adb -t $tid shell "getprop ro.product.model" 2> /dev/null)
+    set model (string trim -c '' $model) # important to strip ^M
 
     # https://unix.stackexchange.com/questions/317330/check-a-range-with-switch-case-in-fish-shell
     if string match --ignore-case --regex 'command not found*' $serialno > /dev/null
 	echo "scrcpy: tid $tid is not supported!"
 	return
     end
-
-    set cmdscrcpy ~/OneDrive/archive/scrcpy-win64-v2.4/scrcpy-win64-v2.4/scrcpy.exe
-    set screen_record_dir c:\\yama\\OneDrive\\tmp\\scrcpy_record
-    set screen_record_file $screen_record_dir/$model-(date +'%Y%m%d-%H%M%S').mp4
 
     echo record to $screen_record_file
 
