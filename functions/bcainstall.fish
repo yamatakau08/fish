775328f4e1fc6bc16a74e8093c10fd76b75f518f
yamatakau08 <yamatakau08@gmail.com>
2024-10-15 14:33:09 +0900

    refactor

--- functions/bcainstall.fish
+++ functions/bcainstall.fish
@@ -1,14 +1,52 @@
 # BCA install
 function bcainstall
-  set package_name "jp.co.sony.hes.home"
+    argparse h/help clean apk= -- $argv
+    or return
 
-  set option $argv[1]
-  set package_file $argv[2]
+    set cmd_name (status current-command)
 
-  if [ $option = "clean" ] || [ $option = "overwrite" ]
-      pkginstall $option $package_name $package_file
-  else
-      echo "Usage: bcainstall clean|overwrite file.apk"
-  end
+    if set -ql _flag_help
+	echo "Usage: $cmd_name [-c|--clean] apk"
+	return 0
+    end
+
+    if set -ql _flag_apk
+	set apk $_flag_apk
+    else
+	echo "Usage: $name [-c|--clean] apk"
+	return 0
+    end
+
+    set output (adb devices -l | tee /dev/tty | sed 's/\r//')
+
+    # check the number of the adb devices found
+    # if device is not found, need to return
+    if ! string match --regex 'transport' $output > /dev/null
+	echo "adb devices not found"
+	return 0
+    end
+
+    while read -l tid --prompt-str="select transport_id: " \
+	or return 1
+
+	if string match --regex '^[1-9]\d*' $tid > /dev/null # tid is digit
+	    break
+	else
+	    continue
+	end
+    end
+
+    set id $tid
+
+    if set -ql _flag_clean
+	set package_name "jp.co.sony.hes.home"
+	echo "uninstalling... $package_name"
+	adb -t $tid uninstall $package_name 2> /dev/null
+	echo "clean install"
+	adb -t $tid install $apk
+    else
+	echo "overwrite"
+	adb -t $tid install -r $apk
+    end
 
 end
