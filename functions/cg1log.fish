84fc68439b80e7819a97a2c7ab4f48cf15dfa40e
yamatakau08 <yamatakau08@gmail.com>
2024-10-16 17:53:17 +0900

    delete this alias

--- functions/cg1log.fish
+++
@@ -1,4 +0,0 @@
-function cg1log --wraps='myteraterm --window_title CG1 --speed 921600 --model CG1' --wraps='myteraterm --window_title CG1 --speed 921600' --wraps='myteraterm --model CG1 --speed 921600' --description 'alias cg1log myteraterm --model CG1 --speed 921600'
-  myteraterm --model CG1 --speed 921600 $argv
-        
-end
