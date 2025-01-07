5dd8470df6d57fb5402e25247e42fb45889e4a15
yamatakau08 <yamatakau08@gmail.com>
2025-01-07 17:33:19 +0900

    fix the blocking in case of adb command target file doesn't exist.

--- functions/myadbdevice.fish
+++ functions/myadbdevice.fish
@@ -2,14 +2,23 @@ function myadbdevice
     set device_serials (adb devices -l | sed -n '2,$ s/^\([^ ]\+\) \+.*/\1/p')
 
     for serial in $device_serials
-	echo $serial
-	adb -s $serial root
+	echo adb device serial: $serial
+	adb -s $serial root > /dev/null
+
 	adb -s $serial shell getprop ro.build.fingerprint
-	adb -s $serial shell cat /mnt/vendor/persist/$company/product_config/Header.json
-	echo
-	#adb -s $serial shell cat /proc/cmdline | sed -e s's/\r//' -e 's/ /\n/g'
-	adb -s $serial shell cat /proc/cmdline | sed -e 's/ /\n/g'
-	echo
+
+	set -l cmdline_file /proc/cmdline
+	set -l output (adb -s $serial shell ls $cmdline_file | sed 's/\r//')
+	if string match $output $cmdline_file > /dev/null
+	    adb -s $serial shell cat /proc/cmdline | sed -e 's/ /\n/g'
+	end
+
+	set -l header_json_file /mnt/vendor/persist/$company/product_config/Header.json
+	set -l output (adb -s $serial shell ls $header_json_file)
+	if string match $output $header_json_file > /dev/null
+	    adb -s $serial shell cat $header_json_file
+	end
+
 	echo
     end
 end
